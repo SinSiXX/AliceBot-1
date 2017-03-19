60c365fec0d68fd8c8b4b0fd8465ac065f2eb25a
Yeo Wen Qin <wqyeo@outlook.com>
2017-03-19 16:46:57 +0800

    Created a function to check prime
    Created a function to check for prime numbers.

---
+++ AliceBot/AddOn-Classes/PrimeTest.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliceBot
+{
+    class PrimeTest : Arithmetics
+    {
+        int NoOfTrials = 50;
+
+        Random randy = new Random();
+        Arithmetics Arith = new Arithmetics();
+        MathFunctions MFunct = new MathFunctions();
+
+    private ulong FastMod(ulong factor, ulong power, ulong modulus){
+            ulong result = 1;
+    while(power > 0){
+        if (power % 2 == 1){
+            result = (result* factor) % modulus;
+            power = power-1;
+        }
+    power = power/2;
+    factor = (factor* factor)%modulus;
+    }
+    return result;
+}
+
+        public bool PrimeTester(ulong inputNum){
+    for (int trial = 0; trial< NoOfTrials; trial++){
+
+                ulong randTest = Convert.ToUInt64(Math.Floor(randy.NextDouble() * (inputNum - 2))) + 2;
+
+        if (gcd(randTest, inputNum) != 1){
+
+        return false;
+        }
+        
+       if (FastMod(randTest, inputNum-1,inputNum)!= 1){
+        return false;
+        }
+        
+    }
+    
+    return true;
+ }
+
+        private ulong gcd(ulong a, ulong b)
+        {
+            ulong c;
+            while (a != 0)
+            {
+                c = a;
+                a = b % a;
+                b = c;
+            }
+            return b;
+        }
+
+
+    }
+}
